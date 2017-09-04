cc735f56f7c627c6199888105b5d6b0b9eb87155
Sahir Vellani <sahir.vellani@gmail.com>
2017-09-04 17:59:59 -0400

    V1.2 Changes: Updated hiding code

--- ContentScripts/on.js
+++ ContentScripts/on.js
@@ -13,6 +13,10 @@ var observer = new MutationObserver(function(mutations){
   mutations.forEach(function(mutation){
     chrome.storage.sync.get('focus', function(item){
       if (item.focus === 'On'){
+        let feedContainer = document.querySelectorAll('[id*="topnews_main_stream"]');
+        for (let i = 0; i < feedContainer.length; i++){
+          feedContainer[i].style.display = 'none';
+        };
         let newPosts = document.querySelectorAll('[data-testid="fbfeed_story"]');
         for (let i = 0; i < newPosts.length; i++){
           newPosts[i].style.display = 'none';
