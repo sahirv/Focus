251298b3f5a19ba2f5e99ff624d9323cc18badbf
Sahir Vellani <sahir.vellani@gmail.com>
2017-09-05 00:30:27 -0400

    big fixes

--- ContentScripts/off.js
+++ ContentScripts/off.js
@@ -2,3 +2,7 @@ var posts = document.querySelectorAll('[data-testid="fbfeed_story"]');
 for (let i = 0; i < posts.length; i++){
   posts[i].style.display = 'block';
 }
+var feedContainer = document.querySelectorAll('[id*="topnews_main_stream"]');
+for (let i = 0; i < feedContainer.length; i++){
+  feedContainer[i].style.display = 'block';
+};
